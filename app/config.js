6ae8260ef789a7defbcf16dfc8fcb8c6ed8f7416
sakilahmmad71 <sakilahmmad71@gmail.com>
2020-09-26 22:16:16 +0600

    Setup configuration file for both development and production server

---
+++ app/config.js
@@ -0,0 +1,22 @@
+const environment = {
+    staging: {
+        httpPort: 3000,
+        httpsPort: 3001,
+        envName: 'staging',
+    },
+    production: {
+        httpPort: 5000,
+        httpsPort: 5001,
+        envName: 'production',
+    },
+};
+
+const currentEnvironment =
+    typeof process.env.NODE_ENV === 'string' ? process.env.NODE_ENV.toLowerCase() : '';
+
+const environmentToExport =
+    typeof environment[currentEnvironment] === 'object'
+        ? environment[currentEnvironment]
+        : environment.staging;
+
+module.exports = environmentToExport;
